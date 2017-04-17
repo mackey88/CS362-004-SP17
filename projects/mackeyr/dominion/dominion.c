1e6732d7c06f7eeaa41bb0d9fc49e28b4825c59b
Ryan Mackey <ryan.mackey@gmail.com>
2017-04-17 01:05:50 -0500

    Move work from assingment1 to master

--- projects/mackeyr/dominion/dominion.c
+++ projects/mackeyr/dominion/dominion.c
@@ -5,6 +5,10 @@
 #include <math.h>
 #include <stdlib.h>
 
+
+
+
+
 int compare(const void* a, const void* b) {
   if (*(int*)a > *(int*)b)
     return 1;
@@ -642,7 +646,55 @@ int getCost(int cardNumber)
 	
   return -1;
 }
+//-----------------------------------------------------------------------------------------
+//Adventurer
+  int playAdventurer(struct gameState *state)
+  {
+    int currentPlayer = whoseTurn(state);
+    int drawntreasure = 0;
+    int cardDrawn;
+    int temphand[MAX_HAND];
+    int z = 0;
+  
 
+     while(drawntreasure<3){
+  if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
+    shuffle(currentPlayer, state);
+  }
+  drawCard(currentPlayer, state);
+  cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
+  if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
+    drawntreasure++;
+  else{
+    temphand[z]=cardDrawn;
+    state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
+    z++;
+  }
+      }
+      while(z-1>=0){
+  state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
+  z=z-1;
+      }
+      return 0;
+  }
+
+//Smithy
+int playSmithy(int handPos, struct gameState *state)
+{
+  int currentPlayer = whoseTurn(state);
+  int i;
+
+   //+3 Cards
+      for (i = 0; i < 3; i++)
+  {
+    drawCard(currentPlayer, state);
+  }
+      
+      //discard card from hand
+      discardCard(handPos, currentPlayer, state, 0);
+      return 0;
+}
+//----------------------------------------------------------------------------------------------------------------------
 int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus)
 {
   int i;
@@ -655,37 +707,21 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-  int drawntreasure=0;
-  int cardDrawn;
-  int z = 0;// this is the counter for the temp hand
+  //int drawntreasure=0;
+  //int cardDrawn;
+  //int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
   }
   
-	
+
+
   //uses switch to select card and perform actions
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+     
+     playAdventurer(state);
 			
     case council_room:
       //+4 Cards
@@ -829,15 +865,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case smithy:
-      //+3 Cards
-      for (i = 0; i < 3; i++)
-	{
-	  drawCard(currentPlayer, state);
-	}
-			
-      //discard card from hand
-      discardCard(handPos, currentPlayer, state, 0);
-      return 0;
+      playSmithy(handPos, state); 
 		
     case village:
       //+1 Card
@@ -1328,6 +1356,7 @@ int updateCoins(int player, struct gameState *state, int bonus)
   return 0;
 }
 
+  
 
 //end of dominion.c
 
